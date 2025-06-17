544832beb26d437389d2436ef7fcbc0a10cc7066
Blaben <blabensolutions@gmail.com>
2025-06-17 06:55:20 +0000

    done

--- 100_Days_of_Learning_Python/Day_one/exercise1.py
+++ 100_Days_of_Learning_Python/Day_one/exercise1.py
@@ -3,6 +3,6 @@
 #Question
 # Ask two questions: persons name and favorite color. Then print a message like " Benjamin Likes Brown"
 
-name = input("What is your name ")
+name = input("What is your name: ")
 fav_color = input("What is your Favourite color ")
 print(name + " likes " + fav_color)
