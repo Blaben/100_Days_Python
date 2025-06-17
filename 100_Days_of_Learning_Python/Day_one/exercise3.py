ec837d3ecb10058d2f7028102650eda8d7d195ec
Blaben <blabensolutions@gmail.com>
2025-06-17 08:04:07 +0000

    age calculator (a)

--- 100_Days_of_Learning_Python/Day_one/exercise3.py
+++ 100_Days_of_Learning_Python/Day_one/exercise3.py
@@ -1,3 +1,4 @@
 # Learning about Variables in Python and requesting users to enter a value using the input function
 
 #Question
+# Write a program to request the year of the user and calculate his/her age
