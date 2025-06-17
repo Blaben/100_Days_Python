5e05068098b67d3f059cefd34ab1055b1d725cdb
Blaben <blabensolutions@gmail.com>
2025-06-17 05:38:15 +0000

    first time working with the input function

---
+++ 100_Days_of_Learning_Python/Day_one/exercise1.py
@@ -0,0 +1,8 @@
+# Learning about Variables in Python and requesting users to enter a value using the input function
+
+#Question
+# Ask two questions: persons name and favorite color. Then print a message like " Benjamin Likes Brown"
+
+name = input("What is your name ")
+fav_color = input("What is your Favourite color ")
+print(name + " likes " + fav_color)
