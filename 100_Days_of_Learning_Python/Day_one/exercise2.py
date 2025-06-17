618e04488dcf024d3f84e53376fd87d73d50d241
Blaben <blabensolutions@gmail.com>
2025-06-17 06:40:46 +0000

    working on weight conversion from lbs to kg

---
+++ 100_Days_of_Learning_Python/Day_one/exercise2.py
@@ -0,0 +1,10 @@
+# Learning about Variables in Python and requesting users to enter a value using the input function
+
+#Question
+# Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+weight = float(input("Enter your weight "))
+weight_in_kg = weight * 0.45
+
+
+print("Your weight in Kilogram is " + weight_in_kg)
